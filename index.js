3eab916cf35a97a1b6dbc56263c092b18893f97a
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-02-17 14:39:44 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name, eventName) {
+    let messages = [];
+    for(let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown() {
+    let number = 10;
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
